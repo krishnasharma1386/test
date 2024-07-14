4a14ccabec28d302e2e8c18595f4c91cfaf1496b
krishnasharma1386 <krishnasharma1386@gmail.com>
2024-07-14 11:26:55 +0530

    Revert "Reapply "retrive back""
    This reverts commit 5e9087a2636c8033210885565f10f8ec55792f84.

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#menu");
-const nav = document.querySelector(".links");
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    nav.classList.toggle('active');
-}
